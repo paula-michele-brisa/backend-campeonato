a80dd1209dd4f88a78c98e13f48bdc41fcda82a9
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-09-20 13:35:47 -0300

    feat(): Criando rotas para os times

--- router/routers.go
+++ router/routers.go
@@ -29,6 +29,48 @@ func routers(router *gin.Engine) {
 			})
 		})
 
+		// Obter o total de times cadastrados
+		v1.GET("/team_count", func(context *gin.Context) {
+			context.JSON(200, gin.H{
+				"total": "10",
+			})
+		})
+
+		// Listar os times cadastrados
+		v1.GET("/teams", func(context *gin.Context) {
+			context.JSON(200, gin.H{
+				"time": "GPS futebol clube",
+			})
+		})
+
+		// Cadastra um novo time
+		v1.POST("/team", func(context *gin.Context) {
+			context.JSON(201, gin.H{
+				"message": "Time Cadastrado",
+			})
+		})
+
+		// Editar/Atualizar time
+		v1.PUT("/team/:id", func(context *gin.Context) {
+			context.JSON(400, gin.H{
+				"message": "Time atualizado",
+			})
+		})
+
+		// Deleta um time
+		v1.DELETE("/time/:id", func(context *gin.Context) {
+			context.JSON(204, gin.H{
+				"message": "Usuário logado",
+			})
+		})
+
+		// Obtem um time pelo id
+		v1.GET("/team/:id", func(context *gin.Context) {
+			context.JSON(200, gin.H{
+				"total": "10",
+			})
+		})
+
 	}
 
 }
