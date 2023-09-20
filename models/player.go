cb8d1bfedf54dc297b71571dbcf1d4cc3bef635d
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-09-20 15:19:36 -0300

    feat(): Criando modelo jogador

---
+++ models/player.go
@@ -0,0 +1,12 @@
+package models
+
+type Player struct {
+	Name     string `json:"name"`
+	Photo    string `json:"photo"`
+	Height   int32  `json:"height"`
+	Weight   int32  `json:"weight"`
+	Age      int32  `json:"age"`
+	Position string `json:"position"`
+	Number   int32  `json:"number"`
+	TeamId   int32  `json:"teamId"`
+}
