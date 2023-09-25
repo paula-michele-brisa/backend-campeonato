263e40178c196484428f3a2242955909c30d5b66
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-09-25 16:09:15 -0300

    refactor(): Utilizando estrutura de tratamento de erro

--- handler/user/registerUserHandler.go
+++ handler/user/registerUserHandler.go
@@ -3,6 +3,7 @@ package user
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/paula-michele-brisa/backend-campeonato/config/rest_err"
 	"github.com/paula-michele-brisa/backend-campeonato/models/request"
 	"net/http"
 )
@@ -13,9 +14,7 @@ func RegisterUserHandler(context *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := context.ShouldBindJSON(&userRequest); err != nil {
-		restErr := err.Error()
-
-		fmt.Sprintf("Campos incorretos, error=%s", restErr)
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("Campos incorretos, error=%s", err))
 
 		context.JSON(http.StatusBadRequest, restErr)
 		return
