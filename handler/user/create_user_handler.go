80ec9db611814fc50d00ee3eca7a2a22bb21a674
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-09-26 10:43:32 -0300

    feat(): Criando estrutura de log

--- handler/user/create_user_handler.go
+++ handler/user/create_user_handler.go
@@ -2,9 +2,11 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/paula-michele-brisa/backend-campeonato/config/logger"
 	"github.com/paula-michele-brisa/backend-campeonato/config/validation"
 	"github.com/paula-michele-brisa/backend-campeonato/handler/models/request"
 	"github.com/paula-michele-brisa/backend-campeonato/model/user"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -14,10 +16,14 @@ var (
 
 // CreateUserHandler é responsável por retorar os dados do usuário
 func CreateUserHandler(context *gin.Context) {
+	logger.Info("Init CreateUser handler",
+		zap.String("journey", "createUser"))
 
 	var userRequest request.UserRequest
 
 	if err := context.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Erro trying to validate user info", err,
+			zap.String("journey", "createUser"))
 		restErr := validation.ValidateUserError(err)
 		context.JSON(restErr.Code, restErr)
 		return
@@ -28,6 +34,9 @@ func CreateUserHandler(context *gin.Context) {
 	if err := domain.CreateUser(); err != nil {
 		context.JSON(err.Code, err)
 	}
+
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"))
 
 	context.String(http.StatusOK, "")
 
--- model/user/create_user.go
+++ model/user/create_user.go
@@ -1,10 +1,14 @@
 package user
 
 import (
+	"github.com/paula-michele-brisa/backend-campeonato/config/logger"
 	"github.com/paula-michele-brisa/backend-campeonato/config/rest_err"
+	"go.uber.org/zap"
 )
 
 func (user *UserDomain) CreateUser() *rest_err.RestErr {
+	logger.Info("Init CreateUser handler",
+		zap.String("journey", "createUser"))
 	user.EncryptPassword()
 	return nil
 }
