a1f78a77ff5302cabc5fb98b8102fde890222568
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-10-02 09:38:27 -0300

    refactor(): Refatorando user de model

--- model/user/user_domain.go
+++ model/user/user_domain.go
@@ -1,32 +1,16 @@
 package user
 
-import (
-	"crypto/md5"
-	"encoding/hex"
-)
-
-type UserDomainInterface interface {
-	GetEmail() string
-	GetPassword() string
-	GetName() string
-	EncryptPassword()
-}
-
-// NewUserDomain construtor do objeto
-func NewUserDomain(email, password, name string) UserDomainInterface {
-	return &userDomain{
-		Name:     name,
-		Password: password,
-		Email:    email,
-	}
-}
-
 // Não pode conter informações da tag (json) porque o Domain não pode ser exportavel
 // Objetos(models response, request) junto ao controller/handler são responsáveis por comunicar com os endpoints
 type userDomain struct {
+	ID       string
 	Email    string
 	Password string
 	Name     string
+}
+
+func (user *userDomain) SetID(id string) {
+	user.ID = id
 }
 
 func (user *userDomain) GetEmail() string {
@@ -39,10 +23,6 @@ func (user *userDomain) GetName() string {
 	return user.Name
 }
 
-// EncryptPassword criptografa a senha do usuário
-func (user *userDomain) EncryptPassword() {
-	hash := md5.New()
-	defer hash.Reset()
-	hash.Write([]byte(user.Password))
-	user.Password = hex.EncodeToString(hash.Sum(nil))
+func (user *userDomain) SetPassword(password string) {
+	user.Password = password
 }
