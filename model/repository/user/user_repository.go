4f2f8076842ae2c4c36c5f8c439952f6579c25bd
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-10-04 16:13:23 -0300

    feat(): Criando service para atualizar dados do usuário

--- model/repository/user/user_repository.go
+++ model/repository/user/user_repository.go
@@ -20,4 +20,5 @@ type UserRepository interface {
 	CreateUser(domainInterface user.UserDomainInterface) (user.UserDomainInterface, *rest_err.RestErr)
 	FindUserByID(id string) (user.UserDomainInterface, *rest_err.RestErr)
 	FindUserByEmail(email string) (user.UserDomainInterface, *rest_err.RestErr)
+	UpdateUser(id string, domainInterface user.UserDomainInterface) *rest_err.RestErr
 }
--- model/service/user/update_use.go
+++ model/service/user/update_use.go
@@ -1,10 +1,22 @@
 package user
 
 import (
+	"github.com/paula-michele-brisa/backend-campeonato/config/logger"
 	"github.com/paula-michele-brisa/backend-campeonato/config/rest_err"
 	"github.com/paula-michele-brisa/backend-campeonato/model/user"
+	"go.uber.org/zap"
 )
 
-func (user *userDomainService) UpdateUser(s string, domainInterface user.UserDomainInterface) *rest_err.RestErr {
+func (user *userDomainService) UpdateUser(id string, userDomain user.UserDomainInterface) *rest_err.RestErr {
+	logger.Info("Init UpdateUser handler",
+		zap.String("journey", "createUser"))
+
+	logger.Info(userDomain.GetPassword())
+	err := user.userRepository.UpdateUser(id, userDomain)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
