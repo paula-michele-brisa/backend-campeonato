20aa23e8ccd51b027de6dd0348531909f089a954
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-10-04 17:18:29 -0300

    feat():  Criando handler para deletar usuário

--- handler/user/delete_user.go
+++ handler/user/delete_user.go
@@ -2,12 +2,24 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/paula-michele-brisa/backend-campeonato/config/logger"
 	"net/http"
 )
 
 // DeleteUserHandler retorna os dados do usuário
 func (userHandler *userHandlerInterface) DeleteUserHandler(context *gin.Context) {
+	userID := context.Param("userId")
 
-	context.JSON(http.StatusOK, "Usuário")
+	err := userHandler.service.DeleteUser(userID)
+
+	if err != nil {
+		if err != nil {
+			logger.Error("Erro trying to call DeleteUserHandler services", err)
+			context.JSON(err.Code, err)
+			return
+		}
+	}
+
+	context.JSON(http.StatusOK, "Usuário excluído com sucesso!")
 
 }
