6b6d314a9497321a73bf0551072896d4c103bb06
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-10-17 13:56:39 -0300

    feat(): Criando player repository

---
+++ repository/player/player_repository_imp.go
@@ -0,0 +1,32 @@
+package player
+
+import (
+	"github.com/paula-michele-brisa/backend-campeonato/config/rest_err"
+	"github.com/paula-michele-brisa/backend-campeonato/domain/player"
+)
+
+func NewPlayerRepository() PlayerRepositoryInterface {
+	return &playerRepository{}
+}
+
+func (player *playerRepository) CreatePlayer(playerDomain player.PlayerDomainInterface) (player.PlayerDomainInterface, *rest_err.RestErr) {
+	return nil, nil
+}
+
+func (player *playerRepository) FindPlayers() ([]player.PlayerDomainInterface, *rest_err.RestErr) {
+	return nil, nil
+}
+
+func (player *playerRepository) FindTotalPlayer() (int32, *rest_err.RestErr) { return 0, nil }
+
+func (player *playerRepository) FindPlayerByID(id string) (int32, *rest_err.RestErr) {
+	return 0, nil
+}
+
+func (player *playerRepository) UpdatePlayer(id string, playerDomain player.PlayerDomainInterface) *rest_err.RestErr {
+	return nil
+}
+
+func (player *playerRepository) DeletePlayer(id string) *rest_err.RestErr {
+	return nil
+}
---
+++ repository/player/player_repository_interface.go
@@ -0,0 +1,20 @@
+package player
+
+import (
+	"github.com/paula-michele-brisa/backend-campeonato/config/rest_err"
+	"github.com/paula-michele-brisa/backend-campeonato/domain/player"
+)
+
+type PlayerRepositoryInterface interface {
+	CreatePlayer(playerDomain player.PlayerDomainInterface) (player.PlayerDomainInterface, *rest_err.RestErr)
+
+	FindPlayers() ([]player.PlayerDomainInterface, *rest_err.RestErr)
+
+	FindTotalPlayer() (int32, *rest_err.RestErr)
+
+	FindPlayerByID(id string) (int32, *rest_err.RestErr)
+
+	UpdatePlayer(id string, playerDomain player.PlayerDomainInterface) *rest_err.RestErr
+
+	DeletePlayer(id string) *rest_err.RestErr
+}
