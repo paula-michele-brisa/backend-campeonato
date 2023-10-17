b2a1187775a018d9614d95ef9079321e5cc65a47
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-10-17 14:34:52 -0300

    feat(): Criando playerEntity

---
+++ entity/player/player_entity.go
@@ -0,0 +1,13 @@
+package player
+
+type PlayerEntity struct {
+	Id       string
+	Name     string
+	Photo    string
+	Height   int32
+	Weight   int32
+	Age      int32
+	Position string
+	Number   int32
+	TeamID   string
+}
